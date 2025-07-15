29989eebe5d83020cc3a8b235ed2de524411198b
Ibrahim <abe4x4@gmail.com>
2025-07-15 14:41:01 -0400

    Fix NameError: httplib2 not defined

--- email_handler.py
+++ email_handler.py
@@ -28,6 +28,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import httplib2
 from reportlab.lib.pagesizes import letter
 
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
