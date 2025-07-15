ec6db95dbb351c5f62a8aa1c343cc730784587c4
Ibrahim <abe4x4@gmail.com>
2025-07-15 13:12:18 -0400

    Increase Gmail API request timeout to prevent TimeoutError

--- email_handler.py
+++ email_handler.py
@@ -28,6 +28,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import httplib2
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
@@ -62,7 +63,7 @@ def get_gmail_service():
         with open("token.json", "w") as token:
             token.write(creds.to_json())
 
-    return build("gmail", "v1", credentials=creds)
+    return build("gmail", "v1", credentials=creds, http=httplib2.Http(timeout=90))
 
 
 def search_emails(service, query):
